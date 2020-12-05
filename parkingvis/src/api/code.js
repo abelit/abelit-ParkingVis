6fc35dd91a50a8a2bc01416f372195ac6134b133
Abelit <ychenid@live.com>
2020-12-05 17:20:35 +0800

    add randomData

---
+++ parkingvis/src/api/code.js
@@ -0,0 +1,129 @@
+const basecode = {
+  "data": [
+    { "code": 520100, "level": "city", "name": "贵阳市" },
+    { "code": 520200, "level": "city", "name": "六盘水市" },
+    { "code": 520300, "level": "city", "name": "遵义市" },
+    { "code": 520400, "level": "city", "name": "安顺市" },
+    { "code": 520500, "level": "city", "name": "毕节市" },
+    { "code": 520600, "level": "city", "name": "铜仁市" },
+    { "code": 522300, "level": "city", "name": "黔西南布依族苗族自治州" },
+    { "code": 522600, "level": "city", "name": "黔东南苗族侗族自治州" },
+    { "code": 522700, "level": "city", "name": "黔南布依族苗族自治州" },
+    { "code": 520502, "level": "district", "name": "七星关区" },
+    { "code": 520521, "level": "district", "name": "大方县" },
+    { "code": 520522, "level": "district", "name": "黔西县" },
+    { "code": 520523, "level": "district", "name": "金沙县" },
+    { "code": 520524, "level": "district", "name": "织金县" },
+    { "code": 520525, "level": "district", "name": "纳雍县" },
+    { "code": 520526, "level": "district", "name": "威宁彝族回族苗族自治县" },
+    { "code": 520527, "level": "district", "name": "赫章县" },
+    { "code": 522701, "level": "district", "name": "都匀市" },
+    { "code": 522702, "level": "district", "name": "福泉市" },
+    { "code": 522722, "level": "district", "name": "荔波县" },
+    { "code": 522723, "level": "district", "name": "贵定县" },
+    { "code": 522725, "level": "district", "name": "瓮安县" },
+    { "code": 522726, "level": "district", "name": "独山县" },
+    { "code": 522727, "level": "district", "name": "平塘县" },
+    { "code": 522728, "level": "district", "name": "罗甸县" },
+    { "code": 522729, "level": "district", "name": "长顺县" },
+    { "code": 522730, "level": "district", "name": "龙里县" },
+    { "code": 522731, "level": "district", "name": "惠水县" },
+    { "code": 522732, "level": "district", "name": "三都水族自治县" },
+    { "code": 520302, "level": "district", "name": "红花岗区" },
+    { "code": 520303, "level": "district", "name": "汇川区" },
+    { "code": 520304, "level": "district", "name": "播州区" },
+    { "code": 520322, "level": "district", "name": "桐梓县" },
+    { "code": 520323, "level": "district", "name": "绥阳县" },
+    { "code": 520324, "level": "district", "name": "正安县" },
+    { "code": 520325, "level": "district", "name": "道真仡佬族苗族自治县" },
+    { "code": 520326, "level": "district", "name": "务川仡佬族苗族自治县" },
+    { "code": 520327, "level": "district", "name": "凤冈县" },
+    { "code": 520328, "level": "district", "name": "湄潭县" },
+    { "code": 520329, "level": "district", "name": "余庆县" },
+    { "code": 520330, "level": "district", "name": "习水县" },
+    { "code": 520381, "level": "district", "name": "赤水市" },
+    { "code": 520382, "level": "district", "name": "仁怀市" },
+    { "code": 520402, "level": "district", "name": "西秀区" },
+    { "code": 520403, "level": "district", "name": "平坝区" },
+    { "code": 520422, "level": "district", "name": "普定县" },
+    { "code": 520423, "level": "district", "name": "镇宁布依族苗族自治县" },
+    { "code": 520424, "level": "district", "name": "关岭布依族苗族自治县" },
+    { "code": 520425, "level": "district", "name": "紫云苗族布依族自治县" },
+    { "code": 520102, "level": "district", "name": "南明区" },
+    { "code": 520103, "level": "district", "name": "云岩区" },
+    { "code": 520111, "level": "district", "name": "花溪区" },
+    { "code": 520112, "level": "district", "name": "乌当区" },
+    { "code": 520113, "level": "district", "name": "白云区" },
+    { "code": 520115, "level": "district", "name": "观山湖区" },
+    { "code": 520121, "level": "district", "name": "开阳县" },
+    { "code": 520122, "level": "district", "name": "息烽县" },
+    { "code": 520123, "level": "district", "name": "修文县" },
+    { "code": 520181, "level": "district", "name": "清镇市" },
+    { "code": 520602, "level": "district", "name": "碧江区" },
+    { "code": 520603, "level": "district", "name": "万山区" },
+    { "code": 520621, "level": "district", "name": "江口县" },
+    { "code": 520622, "level": "district", "name": "玉屏侗族自治县" },
+    { "code": 520623, "level": "district", "name": "石阡县" },
+    { "code": 520624, "level": "district", "name": "思南县" },
+    { "code": 520625, "level": "district", "name": "印江土家族苗族自治县" },
+    { "code": 520626, "level": "district", "name": "德江县" },
+    { "code": 520627, "level": "district", "name": "沿河土家族自治县" },
+    { "code": 520628, "level": "district", "name": "松桃苗族自治县" },
+    { "code": 520201, "level": "district", "name": "钟山区" },
+    { "code": 520203, "level": "district", "name": "六枝特区" },
+    { "code": 520221, "level": "district", "name": "水城县" },
+    { "code": 520281, "level": "district", "name": "盘州市" },
+    { "code": 522301, "level": "district", "name": "兴义市" },
+    { "code": 522302, "level": "district", "name": "兴仁市" },
+    { "code": 522323, "level": "district", "name": "普安县" },
+    { "code": 522324, "level": "district", "name": "晴隆县" },
+    { "code": 522325, "level": "district", "name": "贞丰县" },
+    { "code": 522326, "level": "district", "name": "望谟县" },
+    { "code": 522327, "level": "district", "name": "册亨县" },
+    { "code": 522328, "level": "district", "name": "安龙县" },
+    { "code": 522601, "level": "district", "name": "凯里市" },
+    { "code": 522622, "level": "district", "name": "黄平县" },
+    { "code": 522623, "level": "district", "name": "施秉县" },
+    { "code": 522624, "level": "district", "name": "三穗县" },
+    { "code": 522625, "level": "district", "name": "镇远县" },
+    { "code": 522626, "level": "district", "name": "岑巩县" },
+    { "code": 522627, "level": "district", "name": "天柱县" },
+    { "code": 522628, "level": "district", "name": "锦屏县" },
+    { "code": 522629, "level": "district", "name": "剑河县" },
+    { "code": 522630, "level": "district", "name": "台江县" },
+    { "code": 522631, "level": "district", "name": "黎平县" },
+    { "code": 522632, "level": "district", "name": "榕江县" },
+    { "code": 522633, "level": "district", "name": "从江县" },
+    { "code": 522634, "level": "district", "name": "雷山县" },
+    { "code": 522635, "level": "district", "name": "麻江县" },
+    { "code": 522636, "level": "district", "name": "丹寨县" }
+  ]
+}
+
+function randomExtend(minNum, maxNum) {
+  if (arguments.length === 1) {
+    return parseInt(Math.random() * minNum + 1, 10);
+  } else {
+    return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+  }
+}
+
+let gzCode = {}
+
+basecode.data.forEach(item => {
+  gzCode[item.code] = {province: '贵州',country: item.name,carpark: item.code.toString().substring(item.code.toString().length - 2) == "00" ?randomExtend(20,50): randomExtend(5,10),park: item.code.toString().substring(item.code.toString().length - 2) == "00"?randomExtend(8000,12000):randomExtend(2000,3000),parked:item.code.toString().substring(item.code.toString().length - 2) == "00"?randomExtend(6500,8000): randomExtend(1000,2000)}
+});
+
+gzCode['520100'].carpark = 100
+gzCode['520100'].park = 20000
+gzCode['520100'].parked = 18200
+
+gzCode['520200'].carpark = 70
+gzCode['520200'].park = 17000
+gzCode['520200'].parked = 16000
+
+gzCode['520300'].carpark = 62
+gzCode['520300'].park = 16000
+gzCode['520300'].parked = 13000
+
+export default gzCode;
